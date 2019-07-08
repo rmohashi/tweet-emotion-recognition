518b1d3707982aa3e9a716635b68d552dcdb5ab9
Rodrigo Ohashi <rodrigo.ohashi@infosimples.com.br>
2019-07-08 10:45:58 -0300

    Concat datasets based on the folder content

--- data_fetch/concat_datasets.py
+++ data_fetch/concat_datasets.py
@@ -2,29 +2,28 @@ import os
 import pandas as pd
 from pathlib import Path
 
-def concat_datasets(query, dataset_dir):
+def concat_datasets(dataset_dir):
   DATASET_DIR = Path(dataset_dir).resolve()
-  dir_list = os.listdir(DATASET_DIR)
-  if len(dir_list) > 0:
-    filenames = pd.Series(dir_list, name='files')
-    filenames = filenames[filenames.str.contains(query)]
-    file_count = len(filenames)
-    print('Found {} files with query "'.format(file_count) + query + '"')
-    if file_count > 1:
+  files = pd.Series([x for x in os.listdir(DATASET_DIR) if x.endswith('.csv')],
+                     name='files')
+  if len(files) > 0:
+    queries = files.str.extract(r'(#.+[^.csv]|:.+:)', expand=False)
+    for query in queries.unique():
+      query_files = files[files.str.contains(query)]
+      print('Found {} file(s) with query "'.format(len(query_files)) + query + '"')
       df = pd.DataFrame(columns=['id', 'date', 'user', 'text'])
-      for filename in filenames:
+      for filename in query_files:
         filepath = os.path.join(DATASET_DIR, filename)
         df = pd.concat([df, pd.read_csv(filepath)])
         os.remove(filepath)
       name = str(max(df.id)) + '-' + str(min(df.id)) + '_' + query + '.csv'
       df.to_csv(os.path.join(DATASET_DIR, name), index=None)
-      print('Done!')
+  print('Done!')
 
 if __name__ == '__main__':
   from argparse import ArgumentParser
 
   parser = ArgumentParser()
-  parser.add_argument('query', type=str)
   parser.add_argument('dataset_dir', type=str)
 
   args = parser.parse_args()
