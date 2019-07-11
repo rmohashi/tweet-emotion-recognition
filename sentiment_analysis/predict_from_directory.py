79e68afe4585cac65b01abd33d3b01f8e8dc5979
Rodrigo Masaru Ohashi <32220858+rmohashi@users.noreply.github.com>
2019-07-11 16:32:53 -0300

    Update prediction scripts

--- sentiment_analysis/predict_from_directory.py
+++ sentiment_analysis/predict_from_directory.py
@@ -5,35 +5,62 @@ import pickle
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 from nlp.utils import preprocess
+from .models import NLP_MODEL
 
-def predict_from_directory(files_dir, model_file, save_path, text_col='text'):
+def predict_from_directory(files_dir,
+                           model_weights_file,
+                           model_type,
+                           tokenizer_file,
+                           save_path,
+                           embedding_dim=100,
+                           text_col='text'):
   FILES_DIR = Path(files_dir).resolve()
   RELATIONS_FILE = Path(os.path.abspath(__file__), '../query_relations.json').resolve()
 
   with RELATIONS_FILE.open('rb') as file:
     relations = json.load(file)
 
-  model_path = Path(model_file).resolve()
-  with model_path.open('rb') as file:
-    model = pickle.load(file)
+  tokenizer_path = Path(tokenizer_file).resolve()
+  with tokenizer_path.open('rb') as file:
+    tokenizer = pickle.load(file)
 
-  result_data = []
+  weights_path = Path(model_weights_file).resolve()
+  input_dim = min(tokenizer.num_words, len(tokenizer.word_index) + 1)
+  model = NLP_MODEL[model_type](100, input_dim, embedding_dim=embedding_dim)
+  model.load_weights(weights_path.as_posix())
+
+  emotion_data_dict = {}
 
   for filename in os.listdir(FILES_DIR):
     print('Reading file: "' + filename + '"')
-    file_data = pd.read_csv(os.path.join(FILES_DIR, filename))
-    cleaned_text = preprocess(file_data[text_col])
-
-    result = model.predict(cleaned_text)
-    result = result == 4 if np.bincount(result).argmax() == 4 else result == 0
-    file_data = file_data[result]
 
     query = re.findall(r'(#[^.]+|:.+:)', filename)[0]
-    file_data['label'] = relations[query]
+    emotion = relations[query]
 
-    result_data = result_data + [file_data]
+    file_data = pd.read_csv(os.path.join(FILES_DIR, filename))
+    dict_data = emotion_data_dict[emotion] if emotion in emotion_data_dict else None
+    emotion_data_dict[emotion] = pd.concat([dict_data, file_data])
+
+  result_data = []
+
+  for emotion, dataset in emotion_data_dict.items():
+    print('Processing "' + emotion + '" data...')
+
+    cleaned_texts = preprocess(dataset[text_col])
+    predict_sequences = [text.split() for text in cleaned_texts]
+    list_tokenized_predict = tokenizer.texts_to_sequences(predict_sequences)
+    x_predict = pad_sequences(list_tokenized_predict, maxlen=100)
+
+    result = model.predict(x_predict)
+    mean = np.mean(result)
+    std = np.std(result)
+    dataset = dataset[np.all([(result >= mean - std), (result <= mean - std)], axis=0)]
+    dataset['label'] = emotion
+
+    result_data = result_data + [dataset]
 
   if len(result_data) > 0:
     result_data = pd.concat(result_data)
@@ -48,8 +75,11 @@ if __name__ == '__main__':
 
   parser = ArgumentParser()
   parser.add_argument('files_dir', type=str)
-  parser.add_argument('model_file', type=str)
+  parser.add_argument('model_weights_file', type=str)
+  parser.add_argument('model_type', type=str, choices=['lstm', 'lstm_conv'])
+  parser.add_argument('tokenizer_file', type=str)
   parser.add_argument('save_path', type=str)
+  parser.add_argument('-e', '--embedding_dim', type=int, default=100)
   parser.add_argument('-t', '--text_col', type=str, default='text')
 
   args = parser.parse_args()
--- sentiment_analysis/predict_nb.py
+++ sentiment_analysis/predict_nb.py
@@ -1,39 +1,56 @@
 import os
+import re
+import json
 import pickle
+import numpy as np
 import pandas as pd
 from pathlib import Path
 
 from nlp.utils import preprocess
 
-def predict_nb(data_file, model_file, save_dir=None, text_col='text', positive=False):
-  file_path = Path(data_file).resolve()
-  data = pd.read_csv(file_path)
-  cleaned_text = preprocess(data[text_col])
+def predict_nb(files_dir, model_file, save_path, text_col='text'):
+  FILES_DIR = Path(files_dir).resolve()
+  RELATIONS_FILE = Path(os.path.abspath(__file__), '../query_relations.json').resolve()
+
+  with RELATIONS_FILE.open('rb') as file:
+    relations = json.load(file)
 
   model_path = Path(model_file).resolve()
   with model_path.open('rb') as file:
     model = pickle.load(file)
 
-  result = model.predict(cleaned_text)
-  result = result == 4 if positive else result == 0
-  result_data = data[result]
+  result_data = []
 
-  if save_dir:
-    save_path = Path(save_dir, file_path.name).resolve()
-    result_data.to_csv(save_path, index=None)
-    print('File saved under "' + save_path.as_posix() + '"')
-  else:
-    return result_data
+  for filename in os.listdir(FILES_DIR):
+    print('Reading file: "' + filename + '"')
+    file_data = pd.read_csv(os.path.join(FILES_DIR, filename))
+    cleaned_text = preprocess(file_data[text_col])
+
+    result = model.predict(cleaned_text)
+    result = result == 4 if np.bincount(result).argmax() == 4 else result == 0
+    file_data = file_data[result]
+
+    query = re.findall(r'(#[^.]+|:.+:)', filename)[0]
+    file_data['label'] = relations[query]
+
+    result_data = result_data + [file_data]
+
+  if len(result_data) > 0:
+    result_data = pd.concat(result_data)
+
+    path = Path(save_path).resolve()
+    result_data.to_csv(path, index=None)
+
+    print('Files saved under "' + save_path + '"')
 
 if __name__ == '__main__':
   from argparse import ArgumentParser
 
   parser = ArgumentParser()
-  parser.add_argument('data_file', type=str)
+  parser.add_argument('files_dir', type=str)
   parser.add_argument('model_file', type=str)
-  parser.add_argument('save_dir', type=str)
+  parser.add_argument('save_path', type=str)
   parser.add_argument('-t', '--text_col', type=str, default='text')
-  parser.add_argument('-p', '--positive', action='store_true', default=False)
 
   args = parser.parse_args()
   predict_nb(**vars(args))
