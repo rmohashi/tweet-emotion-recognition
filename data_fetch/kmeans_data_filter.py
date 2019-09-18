a4e149c8f1c84aa8db13d1f17dde9b3c17419e0d
Rodrigo Masaru Ohashi <32220858+rmohashi@users.noreply.github.com>
2019-09-18 15:29:02 -0300

    Use KMeans instead of MiniBatchKMeans

--- data_fetch/kmeans_data_filter.py
+++ data_fetch/kmeans_data_filter.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from nlp.utils import preprocess
 
-from sklearn.cluster import MiniBatchKMeans
+from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
@@ -16,11 +16,11 @@ from sklearn.manifold import TSNE
 @click.option('--output_dir_path', '-od', default='datasets/kmeans', type=str)
 @click.option('--max_features', '-mf', default=8000, type=int)
 @click.option('--n_clusters', '-n', default=4, type=int)
-@click.option('--init_size', '-i', default=1024, type=int)
-@click.option('--batch_size', '-b', default=2048, type=int)
+@click.option('--n_init', '-ni', default=10, type=int)
+@click.option('--max_iter', '-mi', default=300, type=int)
 @click.option('--random_state', '-r', default=20, type=int)
 def kmeans_data_filter(file, output_name, output_dir_path, max_features,
-                       n_clusters, init_size, batch_size, random_state):
+                       n_clusters, n_init, max_iter, random_state):
   filepath = Path(file).resolve()
   df = pd.read_csv(filepath)
 
@@ -29,15 +29,17 @@ def kmeans_data_filter(file, output_name, output_dir_path, max_features,
   tfidf_vectorizer = TfidfVectorizer(
     min_df = 3,
     max_df = 0.95,
-    max_features = 8000
+    max_features = max_features
   )
   tfidf = tfidf_vectorizer.fit_transform(preprocessed_data)
 
-  clusters = MiniBatchKMeans(
-    n_clusters=8,
-    init_size=1024,
-    batch_size=2048,
-    random_state=20
+  print('Running K-means algorithm...')
+
+  clusters = KMeans(
+    n_clusters=n_clusters,
+    n_init=n_init,
+    max_iter=max_iter,
+    random_state=random_state
   ).fit_predict(tfidf)
 
   cluster_to_use = np.bincount(clusters).argmax()
